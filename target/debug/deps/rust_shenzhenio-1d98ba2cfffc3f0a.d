ce20d0d51c07166bc86c85da24ebc502416d5d2b
cristobers <72763707+cristobers@users.noreply.github.com>
2025-03-11 20:00:43 +0000

    new slate

--- target/debug/deps/rust_shenzhenio-1d98ba2cfffc3f0a.d
+++
@@ -1,7 +0,0 @@
-/home/cris/rust-shenzhenio/target/debug/deps/rust_shenzhenio-1d98ba2cfffc3f0a: src/main.rs src/grammar.rs src/register.rs
-
-/home/cris/rust-shenzhenio/target/debug/deps/rust_shenzhenio-1d98ba2cfffc3f0a.d: src/main.rs src/grammar.rs src/register.rs
-
-src/main.rs:
-src/grammar.rs:
-src/register.rs:
--- target/debug/rust-shenzhenio.d
+++
@@ -1,1 +0,0 @@
-/home/cris/rust-shenzhenio/target/debug/rust-shenzhenio: /home/cris/rust-shenzhenio/src/grammar.rs /home/cris/rust-shenzhenio/src/main.rs /home/cris/rust-shenzhenio/src/register.rs
